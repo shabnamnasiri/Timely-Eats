3fffd42d14d8f2809045acee5f8133bffbfeb828
rezn1ck <hollowp1e23@gmail.com>
2026-03-14 16:32:33 +0200

    KAN-17, KAN-18,  KAN-19

---
+++ app/auth/auth.py
@@ -0,0 +1,35 @@
+from flask import session, redirect
+from functools import wraps
+
+
+def login_required(f):
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+
+        if "user_id" not in session:
+            return redirect("/login")
+
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def role_required(role_id):
+
+    def decorator(f):
+
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+
+            if "user_id" not in session:
+                return redirect("/login")
+
+            if session["role_id"] != role_id:
+                return "Access denied"
+
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return decorator
---
+++ app/auth/login/Sign_in.py
@@ -0,0 +1,43 @@
+from flask import render_template, request, redirect, session
+from werkzeug.security import check_password_hash
+
+
+def register_login_routes(app, mysql):
+
+    @app.route("/signin", methods=["GET","POST"])
+    def signin():
+
+        if "user_id" in session:
+            
+            return redirect("/menu")
+
+        if request.method == "POST":
+
+            username_phone = request.form["username_phone"]
+            password = request.form["password"]
+
+            cursor = mysql.connection.cursor()
+
+            cursor.execute(
+                "SELECT user_id, password, role_id FROM User WHERE username=%s OR phone_number=%s",
+                (username_phone, username_phone)
+            )
+
+            user = cursor.fetchone()
+
+            cursor.close()
+
+            if user and check_password_hash(user[1], password):
+
+                session["user_id"] = user[0]
+                session["role_id"] = user[2]
+                if user[2]==1:
+                    return redirect("/menu")
+                elif user[2]==2:
+                    return redirect("/add_menu")
+                else: 
+                    return redirect("/admin")
+
+            return "Invalid username or password"
+
+        return render_template("Sign_in.html")
