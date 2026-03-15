7b006b618bde5e932eebcc5784392229b0dfc5a1
rezn1ck <hollowp1e23@gmail.com>
2026-03-15 13:36:38 +0200

    Update run.py

--- run.py
+++ run.py
@@ -7,13 +7,13 @@ from app.auth.login.Sign_up import register_register_routes
 from app.customer.menu import register_menu_routes
 from app.staff.add_menu import register_add_menu_routes
 
-# Абсолютный путь к папке templates внутри app/
+
 template_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "app", "templates")
 
 app = Flask(__name__, template_folder=template_path)
 app.secret_key = "secret_key_123"
 
-# ----------------- MySQL config -----------------
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
@@ -22,13 +22,13 @@ app.config['MYSQL_PORT'] = 3306
 
 mysql = MySQL(app)
 
-# ----------------- Подключаем маршруты -----------------
+
 register_login_routes(app, mysql)
 register_register_routes(app, mysql)
 register_menu_routes(app, mysql)
 register_add_menu_routes(app, mysql)
 
-# ----------------- Пример маршрутов -----------------
+
 @app.route("/logout")
 def logout():
     session.clear()
@@ -49,6 +49,6 @@ def staff_page():
 def admin_page():
     return "Admin panel"
 
-# ----------------- Запуск -----------------
+
 if __name__ == "__main__":
     app.run(debug=True)
